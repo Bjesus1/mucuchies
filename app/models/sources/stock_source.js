ec65ff58bedd0669e5b967de26a49ef45ca182d2
Joao Azevedo <joao.c.azevedo@gmail.com>
2014-06-16 16:10:01 +0100

    Fix whitespace issues

--- app/models/sources/stock_source.js
+++ app/models/sources/stock_source.js
@@ -17,17 +17,13 @@ Dashboard.StockSource = Dashboard.PeriodicSource.extend({
     var select = this.get("compNames");
     var query = "select * from yahoo.finance.quotes where symbol in(\""+select+"\")";
     var queryEnv = "store://datatables.org/alltableswithkeys";
-    
+
     var url = "http://query.yahooapis.com/v1/public/yql?q=" +
-      encodeURIComponent(query) + "&env="+encodeURIComponent(queryEnv)+"&format=json";
+      encodeURIComponent(query) + "&env=" + encodeURIComponent(queryEnv) + "&format=json";
 
     $.get(url, function(data) {
-      quotes = [];
       quotes = data.query.results.quote;
-
       callback(quotes);
-
-
     });
   }
 });
