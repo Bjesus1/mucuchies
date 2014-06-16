7e41529be975f07d44a35f0f79e0e0dd0387dd36
Joao Azevedo <joao.c.azevedo@gmail.com>
2014-06-16 16:09:44 +0100

    Remove unused method in StockWidget

--- app/models/widgets/stock_widget.js
+++ app/models/widgets/stock_widget.js
@@ -3,28 +3,10 @@
  */
 Dashboard.StockWidget = Dashboard.Widget.extend({
   sourceData: [],
-    
-  stockerFiller: function(){
-    var c = this.get("content");
-
-    for(var i=0; i < c.lenght; i++){
-
-     c[i].symbol;
-     c[i].LastTradePriceOnly;
-     c[i].PercentChange;
-     c[i].MarketCapitalization;
-
-                                   }
-  return c;
-  }.property("content"),
-
 
   title: 'Ad Tech Stocks',
   showLastUpdated: true,
 
-  
   templateName: 'stock_widget',
   classNames: ['widget', 'widget-stock']
- 
-
 });
