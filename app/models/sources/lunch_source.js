6c29a50c0def1e6ac64b1366b402edf2006eebef
Bruno Silva <jesus.silva.bruno@hotmail.com>
2014-06-23 17:34:07 +0100

    Add tomorrow's lunch option

--- app/models/sources/lunch_source.js
+++ app/models/sources/lunch_source.js
@@ -9,50 +9,70 @@ Dashboard.LunchSource = Dashboard.PeriodicSource.extend({
     var serv = "http://dashboard.int.shiftforward.eu:8090/get?";
     var lessImportantUrl = "https://sigarra.up.pt/feup/pt/mob_eme_geral.cantinas";
     var url = serv + "url=" + encodeURI(lessImportantUrl);
+    $.get(url, function(data) {
+        c = JSON.parse(data);
 
-      $.get(url, function(data) {
-        object = JSON.parse(data);
-        var c = object;
-
-        var lunch = { carne: "", peixe: "",data: ""};
+        var lunch = { meat: "", fish: "", tMeat: "", tFish: "", todaysData: "", tomorrowsData: "" };
 
         var today = new Date(); 
         var dd = today.getDate(); 
         var mm = today.getMonth()+1; //January is 0! 
         var yyyy = today.getFullYear(); 
-            if(dd<10){dd='0'+dd} 
-            if(mm<10){mm='0'+mm} 
+        if( dd < 10 ){ dd = '0' + dd } 
+        if( mm < 10 ){ mm = '0' + mm } 
+        today = dd + '-' + mm + '-' + yyyy; 
 
-        var today = dd+'-'+mm+'-'+yyyy; 
+        var tomorrow = new Date( new Date() + 86400000 );
+        var day = tomorrow.getDate()
+        var month = tomorrow.getMonth() + 1
+        var year = tomorrow.getFullYear()
+        if( day < 10 ){ day = '0' + day } 
+        if( month < 10 ){ month = '0' + month } 
+        tomorrow = day + "-" + month + "-" + year;
         
-        lunch.data=today;
+        lunch.todaysData = today;
+        lunch.tomorrowsData = tomorrow;
+        for(i=0;i<c.length;i++){
 
-            for(i=0;i<c.length;i++){
+          if(c[i].descricao == "Cafetaria - Restaurante FEUP"){
+          
+          for(a=0;a<c[i].ementas.length;a++){
 
-              if(c[i].descricao == "Cafetaria - Restaurante FEUP"){
-    
-                for(a=0;a<c[i].ementas.length;a++){
+            if((c[i].ementas[a].estado !== "Fechado") && (c[i].ementas[a].data==today)){
 
-                  if(c[i].ementas[a].data==today){
-
-                    for(b=0;b<c[i].ementas[a].pratos.length;b++){
+            for(b=0;b<c[i].ementas[a].pratos.length;b++){
         
-                      if(c[i].ementas[a].pratos[b].tipo_descr == "Carne"){
-                      lunch.carne = c[i].ementas[a].pratos[b].descricao;
-                      }
+              if(c[i].ementas[a].pratos[b].tipo_descr == "Carne"){
+              lunch.meat = c[i].ementas[a].pratos[b].descricao;
+              }
 
-                      if(c[i].ementas[a].pratos[b].tipo_descr == "Peixe"){
-                      lunch.peixe = c[i].ementas[a].pratos[b].descricao; 
-                      }
-                    }
-                  }
-                }
+              if(c[i].ementas[a].pratos[b].tipo_descr == "Peixe"){
+              lunch.fish = c[i].ementas[a].pratos[b].descricao; 
               }
             }
-      
+            }
+
+            if((c[i].ementas[a].estado !== "Fechado") && (c[i].ementas[a].data==tomorrow)){
+
+            for(h=0;h<c[i].ementas[a].pratos.length;h++){
+        
+              if(c[i].ementas[a].pratos[h].tipo_descr == "Carne"){
+              lunch.tMeat = c[i].ementas[a].pratos[h].descricao;
+              }
+
+              if(c[i].ementas[a].pratos[h].tipo_descr == "Peixe"){
+              lunch.tFish = c[i].ementas[a].pratos[h].descricao; 
+              }
+            }
+            }else{
+              lunch.tMeat = "Fechado";
+              lunch.tFish = "Fechado";
+            }
+          }
+          }
+        }
+
       callback(lunch);
-
-
     });
   }
 });
--- app/models/widgets/lunch_widget.js
+++ app/models/widgets/lunch_widget.js
@@ -2,21 +2,26 @@
 
 
 Dashboard.LunchWidget = Dashboard.Widget.extend({
-  sourceData: { carne: "", peixe: "", date: "" },
-
-  meatDish: function() {
-  	return this.get('content').carne;
-  }.property('content'),
-
-  fishDish: function() {
-  	return this.get('content').peixe;
-  }.property('content'),
-
-  date: function() {
-    return this.get('content').date;
-  }.property('content'),
-
+  sourceData: { meat: "", fish: "", tMeat: "", tFish: "" },
   title: "Today's Lunch",
+  subTitle: "Tomorrow's Lunch",
+  
+  todaysMeatDish: function() {
+  	return this.get('content').meat;
+  }.property('content'),
+
+  todaysFishDish: function() {
+  	return this.get('content').fish;
+  }.property('content'),
+
+  tomorrowsMeatDish: function() {
+    return this.get('content').tMeat;
+  }.property('content'),
+
+  tomorrowsFishDish: function() {
+    return this.get('content').tFish;
+  }.property('content'),
+
   showLastUpdated: true,
   templateName: 'lunch_widget',
   classNames: ['widget', 'widget-lunch']
